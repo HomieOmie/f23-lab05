bf9a3117021db9a630a296f633a0b5e3c09b8294
Om Patel <opatel@andrew.cmu.edu>
2023-09-27 00:53:47 -0400

    Added API documentation to InheritanceSortedIntList

--- Typescript/src/InheritanceSortedIntList.ts
+++ Typescript/src/InheritanceSortedIntList.ts
@@ -14,6 +14,12 @@ import { SortedIntList } from './hidden/SortedIntListLibrary.js'
 class InheritanceSortedIntList extends SortedIntList {
   private totalAdded: number = 0;
   
+  /**
+   * Adds an integer to the list.
+   *
+   * @param num The integer to be added.
+   * @returns True if the integer is added successfully, false otherwise.
+   */
   override add(num: number): boolean {
     const added = super.add(num);
     if (added) {
@@ -22,6 +28,12 @@ class InheritanceSortedIntList extends SortedIntList {
     return added;
   }
 
+  /**
+   * Adds all integers from the provided IntegerList to the list.
+   *
+   * @param list The IntegerList containing elements to be added.
+   * @returns True if any element is added successfully, false otherwise.
+   */
   override addAll(list: IntegerList): boolean {
     let added = false;
     for (let i = 0; i < list.size(); i++) {
