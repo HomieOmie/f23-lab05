6408ec9e4f4623c26813b626b34afbb5697f739c
Om Patel <opatel@andrew.cmu.edu>
2023-09-27 00:51:43 -0400

    Finished Delegation task for TS

--- Typescript/src/DelegationSortedIntList.ts
+++ Typescript/src/DelegationSortedIntList.ts
@@ -8,8 +8,49 @@
  *
  */
 
-class DelegationSortedIntList {
-  // Write your implementation below with API documentatioin
+import { IntegerList } from "./IntegerList";
+import { SortedIntList } from "./hidden/SortedIntListLibrary";
+
+class DelegationSortedIntList implements IntegerList {
+  private sortedIntList: SortedIntList;
+  private totalAdded: number = 0;
+
+  constructor() {
+    this.sortedIntList = new SortedIntList();
+  }
+
+  add(num: number): boolean {
+    this.totalAdded++;
+    return this.sortedIntList.add(num);
+  }
+
+  addAll(list: IntegerList): boolean {
+    let added = false;
+    for (let i = 0; i < list.size(); i++) {
+      added = this.add(list.get(i)) || added;
+    }
+    return added;
+  }
+
+  get(index: number): number {
+    return this.sortedIntList.get(index);
+  }
+
+  remove(num: number): boolean {
+    return this.sortedIntList.remove(num);
+  }
+
+  removeAll(list: IntegerList): boolean {
+    return this.sortedIntList.removeAll(list);
+  }
+
+  size(): number {
+    return this.sortedIntList.size();
+  }
+
+  getTotalAdded(): number {
+    return this.totalAdded;
+  }
 }
 
 export { DelegationSortedIntList }
