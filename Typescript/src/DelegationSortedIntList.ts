db689f21393d40b4d4a69f3e8551eedb6e6a9994
Om Patel <opatel@andrew.cmu.edu>
2023-09-27 00:55:45 -0400

    Added API documentation to DelegationSortedIntList

--- Typescript/src/DelegationSortedIntList.ts
+++ Typescript/src/DelegationSortedIntList.ts
@@ -15,15 +15,30 @@ class DelegationSortedIntList implements IntegerList {
   private sortedIntList: SortedIntList;
   private totalAdded: number = 0;
 
+  /**
+   * Constructs a new DelegationSortedIntList.
+   */
   constructor() {
     this.sortedIntList = new SortedIntList();
   }
 
+  /**
+   * Adds an integer to the list.
+   *
+   * @param num The integer to be added.
+   * @returns True if the integer is added successfully, false otherwise.
+   */
   add(num: number): boolean {
     this.totalAdded++;
     return this.sortedIntList.add(num);
   }
 
+  /**
+   * Adds all integers from the provided IntegerList to the list.
+   *
+   * @param list The IntegerList containing elements to be added.
+   * @returns True if any element is added successfully, false otherwise.
+   */
   addAll(list: IntegerList): boolean {
     let added = false;
     for (let i = 0; i < list.size(); i++) {
@@ -32,22 +47,50 @@ class DelegationSortedIntList implements IntegerList {
     return added;
   }
 
+  /**
+   * Retrieves the integer at the specified position in the list.
+   *
+   * @param index The index of the element to retrieve.
+   * @returns The element at the specified position in the list.
+   */
   get(index: number): number {
     return this.sortedIntList.get(index);
   }
 
+  /**
+   * Removes the specified integer from the list.
+   *
+   * @param num The integer to be removed.
+   * @returns True if the integer is removed successfully, false otherwise.
+   */
   remove(num: number): boolean {
     return this.sortedIntList.remove(num);
   }
 
+  /**
+   * Removes all integers from the provided IntegerList from the list.
+   *
+   * @param list The IntegerList containing elements to be removed.
+   * @returns True if any element is removed successfully, false otherwise.
+   */
   removeAll(list: IntegerList): boolean {
     return this.sortedIntList.removeAll(list);
   }
 
+  /**
+   * Retrieves the size of the list.
+   *
+   * @returns The number of elements in the list.
+   */
   size(): number {
     return this.sortedIntList.size();
   }
 
+  /**
+   * Retrieves the total number of elements added to the list.
+   *
+   * @returns The total number of elements added to the list.
+   */
   getTotalAdded(): number {
     return this.totalAdded;
   }
